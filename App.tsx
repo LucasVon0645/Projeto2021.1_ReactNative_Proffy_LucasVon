6ef6759f3eeab8ee35ee8b11264b7c6e40af01d4
Lucas Von <lucas.vongarcia@gmail.com>
2021-02-21 12:20:18 -0300

    teste2

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import Landing from './src/pages/Landing/index'
+import Landing from './src/pages/Landing/index.tsx'
 
 export default function App() {
   return (
